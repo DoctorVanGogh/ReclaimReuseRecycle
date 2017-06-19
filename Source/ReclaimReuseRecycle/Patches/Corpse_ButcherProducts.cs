d8d0350725e234d0477c4c53bb574850a7ade293
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-06-19 02:39:50 +0200

    Initial prototype

---
+++ Source/ReclaimReuseRecycle/Patches/Corpse_ButcherProducts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Harmony;
+using Verse;
+
+namespace DoctorVanGogh.ReclaimReuseRecycle {
+
+    [HarmonyPatch(typeof(Corpse), nameof(Corpse.ButcherProducts))]
+    // ReSharper disable once UnusedMember.Global
+    public class Corpse_ButcherProducts {
+
+        // ReSharper disable once UnusedMember.Global
+        public static void Postfix(Corpse __instance, ref IEnumerable<Thing> __result, Pawn butcher, float efficiency) {
+            List<Thing> result = new List<Thing>(__result);
+
+            foreach (Hediff hediff in __instance.InnerPawn.health.hediffSet.hediffs) {
+                if (hediff is Hediff_Implant || hediff is Hediff_AddedPart) {
+                    Thing thing = ThingMaker.MakeThing(hediff.def.spawnThingOnRemoved);
+                    // TODO: set thing hitpoints by part health/butcher skill/chance ??? in the end doesn't matter since reimplanted part health will always be at 100%
+                    result.Add(thing);
+                }                
+            }
+            
+            __result = result;
+        }
+    }
+}
---
+++ Source/ReclaimReuseRecycle/ReclaimReuseRecycle.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Harmony;
+using UnityEngine;
+using Verse;
+
+namespace DoctorVanGogh.ReclaimReuseRecycle {
+    // ReSharper disable once UnusedMember.Global
+    public class ReclaimReuseRecycle : Mod {
+       
+        public ReclaimReuseRecycle(ModContentPack content) : base(content) {
+            HarmonyInstance harmony = HarmonyInstance.Create("DoctorVanGogh.ReclaimReuseRecycle");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());           
+        }
+    }
+}
