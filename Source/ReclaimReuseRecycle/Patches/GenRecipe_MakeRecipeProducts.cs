a26197743d1ba81f11f6876a98208587723ab395
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2017-06-24 13:41:02 +0200

    Roll back pre/post butcher health tracker/modifications
    Switch hooks from GetButcherProduct enumeration to actual butcher job execution

---
+++ Source/ReclaimReuseRecycle/Patches/GenRecipe_MakeRecipeProducts.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using Verse;
+
+namespace DoctorVanGogh.ReclaimReuseRecycle.Patches {
+
+    [HarmonyPatch(typeof(GenRecipe), nameof(GenRecipe.MakeRecipeProducts))]
+    public class GenRecipe_MakeRecipeProducts {
+
+        private static Thing[] Empty = new Thing[0];
+
+        public static void Prefix(ref object __state, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient) {
+            if (recipeDef.specialProducts?.Any(p => p == SpecialProductType.Butchery) == true) {
+                __state = dominantIngredient as Corpse;
+            }
+        }
+
+        public static void Postfix(object __state, ref IEnumerable<Thing> __result, RecipeDef recipeDef, Pawn worker, List<Thing> ingredients, Thing dominantIngredient) {
+            var corpse = __state as Corpse;
+
+            if (corpse != null) {
+                List<Thing> result = new List<Thing>(__result ?? Empty);
+
+                RaceProperties race = corpse.InnerPawn.RaceProps;
+                Pawn_HealthTracker healthTracker = corpse.InnerPawn.health;
+                HediffSet diffSet = healthTracker.hediffSet;
+
+                if (race.IsFlesh) {
+                    foreach (Hediff hediff in diffSet.hediffs.Where(d => d is Hediff_Implant || d is Hediff_AddedPart)) {
+                        result.Add(Util.CreateThing(hediff.def.spawnThingOnRemoved, Util.HitpointsFactor(hediff.Part, diffSet)));                        
+                    }
+                }
+
+                if (race.IsMechanoid) {
+                    foreach (var t in diffSet.GetNotMissingParts().Select(bpr => new {
+                        Part = bpr, Spawn = bpr.def.spawnThingOnRemoved
+                    }).Where(t => t.Spawn != null)) {
+                        result.Add(Util.CreateThing(t.Spawn, Util.HitpointsFactor(t.Part, diffSet)));                        
+                    }
+                }
+
+                __result = result;
+            }
+        }
+    }
+}
